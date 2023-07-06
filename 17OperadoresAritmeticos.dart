cce40efba667431c62e20c02f60ab6c2db3c5099
yaikhor <ramorales2112@gmail.com>
2023-07-06 02:20:47 -0600

    Adding operators folder

---
+++ 17OperadoresAritmeticos.dart
@@ -0,0 +1,10 @@
+void main(){
+
+int suma=1+2+3+4;
+double resta= 6-7+8-5.6;
+int mult=5*8;
+double div=10/2;
+double reciduo=5%3;
+
+print("suma:$suma resta:$resta multiplcacion:$mult division:$div reciduo:$reciduo");
+}
